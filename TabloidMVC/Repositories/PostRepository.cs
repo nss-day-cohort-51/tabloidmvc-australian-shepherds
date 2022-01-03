d262dfb0ed13d1d79e598778adfbdd11b0e85cd7
Brady Williams <williams.cole29@yahoo.com>
2022-01-03 14:06:50 -0600

    Finished issue ticket 42 involving deleting posts

--- TabloidMVC/Repositories/PostRepository.cs
+++ TabloidMVC/Repositories/PostRepository.cs
@@ -162,6 +162,24 @@ namespace TabloidMVC.Repositories
             }
         }
 
+        public void Remove(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Post WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+                
+            }
+        }
+
         private Post NewPostFromReader(SqlDataReader reader)
         {
             return new Post()
