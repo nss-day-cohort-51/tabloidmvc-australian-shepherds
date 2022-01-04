ad887db8c58956b506e215056604b98a60aa3899
joshuastewart417 <joshua.stewart780@gmail.com>
2022-01-04 10:01:01 -0600

    created comment model

---
+++ TabloidMVC/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TabloidMVC.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int UserProfileId { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
