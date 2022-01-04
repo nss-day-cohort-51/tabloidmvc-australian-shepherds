b8250bb4480e38fc9b92208abf8cb4e0701a2374
Kaitlyn Long <katielong11@gmail.com>
2022-01-04 11:15:31 -0600

    completed issue ticket #35

---
+++ TabloidMVC/Repositories/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TabloidMVC.Models;
+using Microsoft.Data.SqlClient;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ITagRepository
+    {
+        List<Tag> GetAllTags();
+        void AddTag(Tag tag);
+    }
+}
---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        public List<Tag> GetAllTags()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id, [Name] 
+                                        FROM Tag";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        tags.Add(new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        });
+                    }
+                    reader.Close();
+
+                    return tags;
+                }
+            }
+        }
+
+        public void AddTag(Tag tag)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @" INSERT INTO Tag ([Name])
+                                         OUTPUT INSERTED.ID
+                                         VALUES (@name)";
+
+                    cmd.Parameters.AddWithValue("@name", tag.Name);
+
+                    tag.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
