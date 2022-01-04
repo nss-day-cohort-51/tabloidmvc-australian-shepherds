8f8c12b6894e219f84faeb49bc5126f99fcd8d1b
Colby Ryan <colbyr.ryan@gmail.com>
2022-01-04 10:21:36 -0600

    Added Comment Repo and Interface

---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+        public List<Comment> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "Select * FROM Comment";
+                    var reader = cmd.ExecuteReader();
+
+                    var comments = new List<Comment>();
+
+                    while (reader.Read())
+                    {
+                        comments.Add(new Comment()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                        });
+                    }
+                    reader.Close();
+
+                    return comments;
+                }
+            }
+        }
+    }
+}
