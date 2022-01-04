644d73cd9c1275fd291292263a2c671bf1ce71dd
Colby Ryan <colbyr.ryan@gmail.com>
2022-01-04 15:40:27 -0600

    Finished Issue Ticket 40

---
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public IActionResult Index()
+        {
+            var categories = _categoryRepository.GetAll();
+            return View(categories);
+        }
+    }
+}
