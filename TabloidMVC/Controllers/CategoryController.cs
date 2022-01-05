1b090b291ea8b3cbf3a3fb4474700ab283e89b7a
Tapolcsanyi <iceike237@gmail.com>
2022-01-05 11:56:05 -0600

    only admins can create categories

--- TabloidMVC/Controllers/CategoryController.cs
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TabloidMVC.Repositories;
+using TabloidMVC.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -13,16 +11,26 @@ using TabloidMVC.Models.ViewModels;
 using TabloidMVC.Repositories;
 using TabloidMVC.Models;
 using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
 
 namespace TabloidMVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly IUserProfileRepository _userProfileRepository;
         private readonly ICategoryRepository _categoryRepository;
 
-        public CategoryController(ICategoryRepository categoryRepository)
+        public CategoryController(ICategoryRepository categoryRepository, IUserProfileRepository userProfileRepository)
         {
             _categoryRepository = categoryRepository;
+            _userProfileRepository = userProfileRepository;
         }
         // GET: CategoryController
         public IActionResult Index()
@@ -40,7 +48,21 @@ namespace TabloidMVC.Controllers
         // GET: CategoryController/Create
         public ActionResult Create()
         {
-            return View();
+            string currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            int id = int.Parse(currentUserId);
+            Console.WriteLine(id);
+            UserProfile user = _userProfileRepository.GetById(id);
+            Console.WriteLine(user);
+            Console.WriteLine(user.UserTypeId);
+
+            if (user.UserTypeId == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
         [HttpPost]
 
